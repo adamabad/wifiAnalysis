7632fbabe70b33bb31586d30e8d2d107eb2e068b
adamabad <73717269+adamabad@users.noreply.github.com>
2021-03-26 15:55:39 -0400

    Final Day Push
    WIP Final push of the day

--- wifiAnalysis/wifiAnalysis/ScanProgressPage.xaml.cs
+++ wifiAnalysis/wifiAnalysis/ScanProgressPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Connectivity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,6 @@ namespace wifiAnalysis
         {
             InitializeComponent();
         }
-        
     }
+    
 }
